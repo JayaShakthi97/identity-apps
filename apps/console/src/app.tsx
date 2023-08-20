b85f8b3f81722a53ec298d0b2657e4bf7c443377
JayaShakthi97 <jayashakthiperera@gmail.com>
2023-08-20 20:13:00 +0530

    b

--- apps/console/src/app.tsx
+++ apps/console/src/app.tsx
@@ -6,7 +6,7 @@
  * in compliance with the License. s
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0 s
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
